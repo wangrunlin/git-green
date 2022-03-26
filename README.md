9bd6d5204b1913b070cfa43356b8b030b73be12c
Leo <leo@wangrunlin.com>
2022-03-27 00:45:42 +0800

    Init git-green

---
+++ README.md
@@ -0,0 +1,3 @@
+# green-github
+
+full green github
---
+++ git-green.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for i in {365..1000};do
+	# write text
+	datetime=`date -v -"$i"d "+%Y-%m-%dT%H:%M:%S"`
+
+	git checkout -b git-green
+
+	echo "$(date "+%Y-%m-%d") add $datetime\n" >> git-green.md
+
+	git add git-green.md
+	git commit -m "Update git-green.md"
+
+	# modify date
+	git commit --amend --date="$datetime" -m "Update git-green.md"
+
+done;
+
+# git push
+
+
